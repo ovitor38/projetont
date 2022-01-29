e8281de0e52549d579711bd18b67daebd90574cd
José Paulo <lorenconi16@gmail.com>
2022-01-29 15:55:19 -0300

    feat: Add novas rotas

--- back/back/src/routes.js
+++ back/back/src/routes.js
@@ -1,23 +1,24 @@
-const { Router } = require('express');
+const { Router } = require("express");
 const register = require('./controllers/registerUsers');
 const login = require('./controllers/loginUser');
+const task = require('./controllers/taskUser');
+const tag = require('./controllers/tagUser');
+const setting = require('./controllers/settingUser');
 const cors = require('cors');
 const authMiddlewares = require('./middlewares/authMiddlewares');
 
 const router = Router();
+router.use(cors());
 
-router.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin","*");
-    res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
-    res.header("Access-Control-Allow-Headers", "X-PINGOTHER, Content-Type, Authorization")
-    router.use(cors());
-    next();
-})
 
-router.post('/register', register.store)
-router.post('/login',  login.index)
-router.use(authMiddlewares)
+router.post("/register", register.store);
+router.post("/login", login.index);
 
-router.get('users', login.show)
+router.use(authMiddlewares);
 
+router.get("users", login.show);
+
+router.post("/task", task.store);
+router.post("/tag", tag.store);
+router.post("/settings", setting.storage);
 module.exports = router;
